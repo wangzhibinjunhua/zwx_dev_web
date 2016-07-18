d3da020c30e36e8369459e964f689563e0313146
wzb <wangzhibin_x@qq.com>
2016-07-18 10:15:10 +0800

    以表格形式显示

--- html/api/gsm/bpbg/get_bpbg_v0.php
+++ html/api/gsm/bpbg/get_bpbg_v0.php
@@ -1,3 +1,10 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 <?php
 require_once('/home/work/wzb/project/release/common/db_config.php');
 $devicetype="";
@@ -26,22 +33,35 @@ if(!$db){
 	die();
 }
 
+
+$type="bp";
 //$sql=mysql_query("insert into WatchLocations (userid,longitude,latitude,datetime) values ($watch_imei,'99.:99','99.22',$watch_time)");
 if($devicetype == '1'){
 	$sql=mysql_query("select * from gsm_bg");
+	$type="bg";
 }else{
 	$sql=mysql_query("select * from gsm_bp");
 }
-while($row=mysql_fetch_assoc($sql))
-$output[]=$row;
-
-mysql_close();
-if(is_null($output)){
-echo json_encode(array('code'=>1008,'message'=>'No data'));
-die();
+echo "<table border=1>";
+echo "<tr><td>ID</td><td>SN</td><td>imsi</td><td>$type</td><td>time</td></tr>";
+while($row=mysql_fetch_array($sql)){
+	$output[]=$row;
+	echo "<tr>";
+	echo "<td>".$row[id]."</td>";
+	echo "<td>".$row[sn]."</td>";
+	echo "<td>".$row[imsi]."</td>";
+	echo "<td>".$row[$type]."</td>";
+	echo "<td>".$row[datetime]."</td>";
+	echo "<td>"."<input type='submit' value='test'/>"."</td>";
+	echo "</tr>";
 }
-print(json_encode($output,true));
 
-
-
+// mysql_close();
+ if(is_null($output)){
+ echo json_encode(array('code'=>1008,'message'=>'No data'));
+ die();
+ }
+// print(json_encode($output,true));
 ?>
+</body>
+</html>
