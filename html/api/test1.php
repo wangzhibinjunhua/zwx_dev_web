aaf572ed920ca72175f27aab28d4716748a6d860
wzb <wangzhibin_x@qq.com>
2016-09-01 16:53:44 +0800

    update 手表接口测试

--- html/api/test1.php
+++ html/api/test1.php
@@ -8,7 +8,8 @@ $msg=$_POST['msg'];
 $client = stream_socket_client('tcp://127.0.0.1:9292');
 if(!$client)exit("can not connect");
 // 模拟超级用户，以文本协议发送数据，注意Text文本协议末尾有换行符（发送的数据中最好有能识别超级用户的字段），这样在Event.php中的onMessage方法中便能收到这个数据，然后做相应的处理即可
-$send_data='{"type":"send","content":'.'"'.$msg.'"'.'}';
+$send_data='{"id":"0000dwdad231adwd","cmd":"test","info":'.'"'.$msg.'"'.'}';
 echo $send_data;
-fwrite($client, $send_data."\n");
+$rs_len=sprintf("%04x",strlen($send_data));
+fwrite($client, $rs_len.$send_data);
 fclose($client);
